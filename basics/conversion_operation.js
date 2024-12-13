54ed84dc353320d0916a25441952c7fdf6c00e73
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-13 22:49:55 +0000

    completed

---
+++ basics/conversion_operation.js
@@ -0,0 +1,28 @@
+let score = "33"
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+/*
+"33" = 33
+"33abc" = NaN
+true = 1
+null = 0
+undefined = NaN
+*/
+ let is_logged_in = 1
+
+ let boolean_is_logged_in = Boolean(is_logged_in)
+
+ console.log(typeof boolean_is_logged_in);
+ console.log(boolean_is_logged_in);
+ 
+/*
+    1 = true
+    0 = false
+    " " = false
+    "piyush" = true
+ */
