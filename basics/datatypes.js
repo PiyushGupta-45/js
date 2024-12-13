f9ad3539671525b9d1c9841f184944a99aeb5268
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-13 22:35:30 +0000

    done

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+let name = "piyush"
+let age = 18
+let isloggedin = true
+let state = null
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(isloggedin))
+console.log(typeof(state))
+
+
+console.log(typeof null)
+console.log(typeof undefined)
