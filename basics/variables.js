9d3a1ab251c4023adff70675b88c1dd786fe1fc4
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-13 20:47:31 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const id = 1245//it is used to make any variable constant through out the code which cant be changed
+let name = "piyush"//it cant be changed throungh out the code if written in the scope
+var email = "piyush@gmail.com"//it can be change through out the code
+phone = 1234567890//it is not a good manner to define the variable
+
+
+console.table([id,name,email,phone])// it can print the output in the tabular form 
