68ad6692eb6238ca56dd43142b215294faa15e23
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-14 01:30:55 +0000

    some more conversions done

---
+++ basics/Some_More_operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(3%2);
+
+let str1 = "hlo"
+let str2 = "piyush"
+
+let str3 = str1+str2
+// console.log(str3)
+
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+ 
