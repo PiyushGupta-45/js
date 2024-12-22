cbaba35ace484abbe9e98a768885d4205b63bef6
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-22 16:19:31 +0000

    stake and heap done

---
+++ basics/stake_and_heap.js
@@ -0,0 +1,12 @@
+// stake (primitive)
+
+//heap (non primitive)
+
+let myname = "piyush"
+
+let anothermyname = myname
+anothermyname = "piyush gupta"
+
+console.log(myname)
+console.log(anothermyname)
+
