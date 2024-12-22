bc9ded0658d4c2378af12538f704df6588a624dc
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-22 16:19:47 +0000

    string

---
+++ basics/strings.js
@@ -0,0 +1,24 @@
+const name = "piyush"
+const age = 19
+// console.log(name+age)
+// console.log(`my name is ${name.toUpperCase()} and age is ${age}`)
+
+// console.log(name[0])
+// console.log(name.length)
+
+// console.log(name.charAt(2))
+// console.log(name.indexOf('p'))
+
+// const newName = name.substring(0,4)
+// console.log(newName)
+
+// const anotherName = name.slice(0,5)
+// console.log(anotherName)
+
+const newstring1= "    piyush      "
+console.log(newstring1)
+console.log(newstring1.trim())
+
+const url = "https://google.com/mdn%20hlo"
+console.log(url.replace('%20','-'))
+
