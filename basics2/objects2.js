e34595ba9a918046d5843360211541464fc16649
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-27 17:08:01 +0000

    objects completed

---
+++ basics2/objects2.js
@@ -0,0 +1,28 @@
+const tinderUser = new Object()
+tinderUser.id = "132"
+tinderUser.name = "piyush"
+tinderUser.isLoggedIn = true
+
+// console.log(tinderUser)
+
+const regular ={
+    email:"eoef",
+    fullname:{
+        userFullname:{
+            firstName : "piyush",
+            lastName : "gupta "
+        }
+    }
+}
+// console.log(regular.fullname.userFullname.firstName)
+
+const obj1 = {1: "A" , 2: "B"}
+const obj2 = {3: "A" , 4: "B"}
+const obj4 = {5: "A" , 6: "B"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({}, obj1,obj2,obj4) 
+// const obj3 = {...obj1,...obj2,...obj4}
+
+// console.log(obj3)
+
