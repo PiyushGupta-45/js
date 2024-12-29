a4d5586d410854c44a25ca0826e14befbd5e663b
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2024-12-29 17:21:08 +0000

    functions done

--- basics3/functions.js
+++ basics3/functions.js
@@ -38,9 +38,43 @@ const result = add2number(3,4)
 
 function loginUSER(username){
     if(username === undefined){
-        console.log("pls enter the value")
+        // console.log("pls enter the value")
         return
     }
     return `${username} just loggedin`
 }
-console.log(loginUSER())
+// console.log(loginUSER())
+
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(2))
+// console.log(calculateCartPrice(200,300,400))
+
+const user = {
+    username:"Piyush",
+    price :100
+}
+
+function handleObject(anyObject){
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username:"wfwf",
+    price :100
+})
+
+const mynewArray=[200,100,400,600]
+
+function returnSecondValue(getarray){
+
+    return getarray[1]
+
+}
+// console.log(returnSecondValue(mynewArray))
+// console.log(returnSecondValue([20,300,562,41]))
