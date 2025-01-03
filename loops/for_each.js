eb100d9ad1596331e1d86662d03c3878fb3b7418
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2025-01-03 16:42:17 +0000

    for loops completed

---
+++ loops/for_each.js
@@ -0,0 +1,20 @@
+const coding = [1,2,3]
+
+coding.forEach(function (item){
+    // console.log(item)
+})
+
+coding.forEach((hlo) =>{
+// console.log(hlo)
+})
+
+function printme(item){
+    console.log(item)
+}
+
+// coding.forEach(printme)
+
+coding.forEach((item,index,arr) =>{
+    // console.log(item,index,arr)
+})
+
